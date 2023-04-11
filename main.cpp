7b87501eb2be15de70a37074f17d8aa49be20247
vladimirsgit <stratulatvladimir@protonmail.com>
2023-04-11 16:58:50 +0300

    moved Polinom.cpp to .h, because of template cpp problems

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
 #include "Polinom.h"
 #include "Complex.h"
-#include "Polinom.cpp"
-//#include "Complex.cpp"
 
 using namespace std;
 int main() {
 
-    Polinom <int> x(3, 1, 2, 3, 4);
-    Complex a(3, 5), b(3, 4), c(2, 6);
-    Polinom <Complex> z(2, a, b, c);
-    cout << z << " ";
-
+    Polinom <int> x(0, 1);
+    cout << x << " ";
     return 0;
 }
